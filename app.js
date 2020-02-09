4b697b85c4b2bc1e1874e32f131b227027a8a9ea
Taishiro Mizutani <taimizutani@Tais-MacBookPro.tocil-05.res.warwick.ac.uk>
2020-02-09 10:56:13 +0000

    first time adding

---
+++ app.js
@@ -0,0 +1,4 @@
+// document.querySelector(".mybannerbtn").addEventListener("click", () =>{
+//     document.querySelector(".mybanner").style.display = "none";
+//     document.querySelector("mydisplay2").style.cssText = "display: ";
+// });
