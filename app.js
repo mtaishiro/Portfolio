cc1a50cdf2b21b191892b330b3152e7014b32a80
Taishiro Mizutani <taimizutani@Tais-MacBookPro.local>
2020-02-19 22:58:10 +0000

    added fadeIn effect using js on projects

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-// document.querySelector(".mybannerbtn").addEventListener("click", () =>{
-//     document.querySelector(".mybanner").style.display = "none";
-//     document.querySelector("mydisplay2").style.cssText = "display: ";
-// });
+var projectshead = document.querySelector(".projectsheading");
+var mycarousel = document.querySelector(".mycarousel");
+var skillshead = document.querySelector(".skillsheading");
+
+function add_animated(){
+    if (window.scrollY>190||document.body>190||window.pageYOffset>190) {
+        projectshead.style.visibility = "visible";
+        projectshead.classList.add("myfadeIn");
+    }
+    if (window.scrollY>560||document.body>560||window.pageYOffset>560) {
+        mycarousel.style.visibility = "visible";
+        mycarousel.classList.add("myfadeIn");
+    }
+    if (window.scrollY>1100||document.body>1100||window.pageYOffset>1100) {
+        skillshead.style.visibility = "visible";
+        skillshead.classList.add("myfadeIn");
+    }
+
+}
